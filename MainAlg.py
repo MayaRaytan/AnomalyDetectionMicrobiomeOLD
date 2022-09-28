f9c1c6e0573873131f323a49e0882261e94c2b18
MayaRaytan <92438533+MayaRaytan@users.noreply.github.com>
2022-09-28 15:49:08 +0300

    Add files via upload

--- MainAlg.py
+++ MainAlg.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 import random
+import sklearn
+pip install scikit-bio
+from skbio import DistanceMatrix
+from skbio.stats.ordination import pcoa
 
 class Tree_Node:
     def __init__(self, depth=0, left=None, right=None, split_att=None, split_val=None, size=0):
@@ -24,24 +28,28 @@ class Forest:
         function: Creates forest of omriTrees.
         output: an omriForest
     '''
-    def fit(self, X,psi, l, t=100):
+    def fit(self, X, OTUs, psi, l, t=100, distance_matrix="de_brui"):
         for i in range(t):
-            self.trees.append(omri_tree(X, 0, l, psi))
+            self.trees.append(omri_tree(X, OTUs, 0, l, psi, distance_matrix))
             self.numberOfTrees += 1
 
 '''
-    inputs: X=input data, OTUs=taxa, e=current tree height, l=height limit, psi=sub sampling size
+    inputs: X=input data, OTUs=taxa, e=current tree height, l=height limit, psi=sub sampling size, distance_matrix = chosen function to calcukate distance matrix
     function:
     output: an omriTree represented by it's root (treeNode type)
 '''
-def omri_tree(X, OTUs, e, l, psi):
+def omri_tree(X, OTUs, e, l, psi, distance_matrix):
     #np.random.seed(0)
     if e >= l or len(X) <= 1:
         return Tree_Node(depth=e, size=len(X))
     else:
-        taxa = X.sample(n=psi, random_state=2)
-        '''Continue from Here!!!!!!!!!!!!!!!!!!!!!!!!'''
+        sampled_X = X.sample(n=psi, random_state=2)
+        renormalized_X = relative_abundence(sampled_X)
+        distance_mat = distance_matrix(renormalized_X)
+        p = skbio.stats.ordination.pcoa(distance_mat)
 
+def distance_matrix():
+    return
 
 def create_data(data):
     data, OTUs = filter_data(data)
@@ -63,3 +71,5 @@ def relative_abundence(data):
 metadata = pd.read_csv("C:\Maya\CS\AnomalyDetectionMicrobiome\Human_Microbiome_Project_1_6_2017_metadata.tab",sep='\t')
 data = pd.read_csv("C:\Maya\CS\AnomalyDetectionMicrobiome\Human_Microbiome_Project_1_6_2017_data.tab",sep='\t')
 X, OTUs = create_data(data)
+
+omri_tree(X, OTUs, 1, 3, 10, distance_matrix)
